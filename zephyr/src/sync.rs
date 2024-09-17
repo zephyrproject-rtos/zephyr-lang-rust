2c79bd2c92e9a5e44949f8f1ca13b08ad436dbc5
David Brown <david.brown@linaro.org>
2024-09-17 17:03:51 -0600

    zephyr: Make symbols from portable-atomic available
    Provide the atomic types  from portable-atomic in
    `zephyr::sync::atomic`, and, if allocation is enabled, provide
    `zephyr::sync::Arc`.  The `alloc::arc` will only be available on Zephyr
    targets that have atomic intrinsics, and the portable one can be
    supported on any Zephyr target.  There are some limitations described in
    the documentation.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

---
+++ zephyr/src/sync.rs
@@ -0,0 +1,24 @@
+//! Higher level synchronization primitives.
+//!
+//! These are modeled after the synchronization primitives in
+//! [`std::sync`](https://doc.rust-lang.org/stable/std/sync/index.html) and those from
+//! [`crossbeam-channel`](https://docs.rs/crossbeam-channel/latest/crossbeam_channel/), in as much
+//! as it makes sense.
+
+pub mod atomic {
+    //! Re-export portable atomic.
+    //!
+    //! Although `core` contains a
+    //! [`sync::atomic`](https://doc.rust-lang.org/stable/core/sync/atomic/index.html) module,
+    //! these are dependent on the target having atomic instructions, and the types are missing
+    //! when the platform cannot support them.  Zephyr, however, does provide atomics on platforms
+    //! that don't support atomic instructions, using spinlocks.  In the Rust-embedded world, this
+    //! is done through the [`portable-atomic`](https://crates.io/crates/portable-atomic) crate,
+    //! which will either just re-export the types from core, or provide an implementation using
+    //! spinlocks when those aren't available.
+
+    pub use portable_atomic::*;
+}
+
+#[cfg(CONFIG_RUST_ALLOC)]
+pub use portable_atomic_util::Arc;
