823b48a2d4c5a8b0d6de0502704f133163b34574
David Brown <david.brown@linaro.org>
2024-10-11 10:46:27 -0600

    zephyr: Add some missing doc comments
    Document a handful of functions so that we can enforce documentation.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- zephyr/src/sys.rs
+++ zephyr/src/sys.rs
@@ -14,5 +14,15 @@ use zephyr_sys::k_timeout_t;
 // These two constants are not able to be captured by bindgen.  It is unlikely that these values
 // would change in the Zephyr headers, but there will be an explicit test to make sure they are
 // correct.
+
+/// Represents a timeout with an infinite delay.
+///
+/// Low-level Zephyr constant.  Calls using this value will wait as long as necessary to perform
+/// the requested operation.
 pub const K_FOREVER: k_timeout_t = k_timeout_t { ticks: -1 };
+
+/// Represents a null timeout delay.
+///
+/// Low-level Zephyr Constant.  Calls using this value will not wait if the operation cannot be
+/// performed immediately.
 pub const K_NO_WAIT: k_timeout_t = k_timeout_t { ticks: 0 };
