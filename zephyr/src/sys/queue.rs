2ca789e14e97cd8b36fe93cbcbc10c83a44d51c9
David Brown <david.brown@linaro.org>
2024-10-24 10:37:06 -0600

    zephyr: sys: queue: Add Queue::new()
    Add support for dynamically allocate sys::Queues.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- zephyr/src/sys/queue.rs
+++ zephyr/src/sys/queue.rs
@@ -5,6 +5,8 @@
 //! implementation of `zephyr::sys::channel`, which can be used without needing unsafe.
 
 use core::ffi::c_void;
+use core::fmt;
+use core::mem;
 
 use zephyr_sys::{
     k_queue,
@@ -13,13 +15,13 @@ use zephyr_sys::{
     k_queue_get,
 };
 
+use crate::error::Result;
 use crate::sys::K_FOREVER;
-use crate::object::{StaticKernelObject, Wrapped};
+use crate::object::{Fixed, StaticKernelObject, Wrapped};
 
 /// A wrapper around a Zephyr `k_queue` object.
-#[derive(Clone, Debug)]
 pub struct Queue {
-    item: *mut k_queue,
+    item: Fixed<k_queue>,
 }
 
 unsafe impl Sync for StaticKernelObject<k_queue> { }
@@ -28,6 +30,21 @@ unsafe impl Sync for Queue { }
 unsafe impl Send for Queue { }
 
 impl Queue {
+    /// Create a new Queue, dynamically allocated.
+    ///
+    /// This Queue can only be used from system threads.
+    ///
+    /// **Note**: When a Queue is dropped, any messages that have been added to the queue will be
+    /// leaked.
+    #[cfg(CONFIG_RUST_ALLOC)]
+    pub fn new() -> Result<Queue> {
+        let item: Fixed<k_queue> = Fixed::new(unsafe { mem::zeroed() });
+        unsafe {
+            k_queue_init(item.get());
+        }
+        Ok(Queue { item })
+    }
+
     /// Append an element to the end of a queue.
     ///
     /// This adds an element to the given [`Queue`].  Zephyr requires the
@@ -37,14 +54,14 @@ impl Queue {
     ///
     /// [`Message`]: crate::sync::channel::Message
     pub unsafe fn send(&self, data: *mut c_void) {
-        k_queue_append(self.item, data)
+        k_queue_append(self.item.get(), data)
     }
 
     /// Get an element from a queue.
     ///
     /// This routine removes the first data item from the [`Queue`].
     pub unsafe fn recv(&self) -> *mut c_void {
-        k_queue_get(self.item, K_FOREVER)
+        k_queue_get(self.item.get(), K_FOREVER)
     }
 }
 
@@ -59,7 +76,7 @@ impl Wrapped for StaticKernelObject<k_queue> {
             k_queue_init(ptr);
         }
         Queue {
-            item: ptr,
+            item: Fixed::Static(ptr),
         }
     }
 }
@@ -77,3 +94,9 @@ impl Wrapped for StaticKernelObject<k_queue> {
 /// my_queue.send(...);
 /// ```
 pub type StaticQueue = StaticKernelObject<k_queue>;
+
+impl fmt::Debug for Queue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "sys::Queue {:?}", self.item.get())
+    }
+}
