c89b2b1a68ab4c9085408bcc2b348ffe66478609
David Brown <david.brown@linaro.org>
2025-01-07 09:50:40 -0700

    zephyr: work: Support showing work in Segger SystemView
    If `CONFIG_SEGGER_SYSVIEW` is enabled, add a few calls to the workqueue
    scheduling to record these as task switches.  There is still a problem
    with the task names showing up, but this does generally show scheduled
    work as separate tasks in System View.
    
    The main issues where this lacks is that if the Zephyr scheduler
    schedules something else, and then returns to the work queue, that will
    be shown as being done on the work queue thread instead of the
    particular work.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- zephyr-sys/build.rs
+++ zephyr-sys/build.rs
@@ -82,6 +82,7 @@ fn main() -> Result<()> {
         .allowlist_function("sys_.*")
         .allowlist_function("z_log.*")
         .allowlist_function("bt_.*")
+        .allowlist_function("SEGGER.*")
         .allowlist_item("E.*")
         .allowlist_item("K_.*")
         .allowlist_item("ZR_.*")
