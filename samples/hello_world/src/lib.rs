ebaa4183f67a815909dda531b1dd158dbe61139d
David Brown <david.brown@linaro.org>
2025-01-16 15:46:40 -0700

    samples: hello_world: Run `cargo fmt`
    Format the source according to the default rules. This provides a
    baseline for future changes to require well-formatted source.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- samples/hello_world/src/lib.rs
+++ samples/hello_world/src/lib.rs
@@ -7,7 +7,9 @@ use log::info;
 
 #[no_mangle]
 extern "C" fn rust_main() {
-    unsafe { zephyr::set_logger().unwrap(); }
+    unsafe {
+        zephyr::set_logger().unwrap();
+    }
 
     info!("Hello world from Rust on {}", zephyr::kconfig::CONFIG_BOARD);
 }
