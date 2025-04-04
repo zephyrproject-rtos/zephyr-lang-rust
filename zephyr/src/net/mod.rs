e0d6d98ec2586849c447600e81f4918b00eb77e1
Matt Rodgers <mrodgers@witekio.com>
2025-04-04 17:14:49 +0100

    zephyr: net: Add TCP implementation
    Add TcpListener and TcpStream structs which work much like those in the
    Rust std library.
    
    Also add a basic test case to confirm that the TCP abstractions have the
    basic functions working.
    
    Signed-off-by: Matt Rodgers <mrodgers@witekio.com>

--- zephyr/src/net/mod.rs
+++ zephyr/src/net/mod.rs
@@ -10,6 +10,7 @@ mod socket;
 
 use crate::error::Result;
 use crate::net::socket::{Domain, Protocol, SockType, Socket};
+use core::ffi::c_int;
 use core::net::SocketAddr;
 
 /// UDP socket.
@@ -26,18 +27,16 @@ pub struct UdpSocket {
 impl UdpSocket {
     /// Create a UDP socket bound to the provided socket address.
     pub fn bind(addr: &SocketAddr) -> Result<Self> {
-        let domain = match addr {
-            SocketAddr::V4(_) => Domain::AfInet,
-            SocketAddr::V6(_) => Domain::AfInet6,
-        };
+        let domain = Domain::from_socket_addr(addr);
 
-        let mut sock = Socket::new(domain, SockType::Dgram, Protocol::IpprotoUdp)?;
+        let mut sock = Socket::new(domain, SockType::Dgram, Protocol::IpProtoUdp)?;
         sock.bind(addr)?;
 
         Ok(Self { sock })
     }
 
-    /// Send data to the specified socket address.
+    /// Send data to the specified socket address, returning the number of bytes that were actually
+    /// sent.
     pub fn send_to(&self, buf: &[u8], addr: &SocketAddr) -> Result<usize> {
         self.sock.send_to(buf, addr)
     }
@@ -45,5 +44,75 @@ impl UdpSocket {
     /// Receive from the socket, returning the data length and peer address it was received from.
     pub fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
         self.sock.recv_from(buf)
+    }
+}
+
+/// TCP Listener
+///
+/// A TCP listener can be created by binding to a local socket address. It represents a server
+/// socket which is ready to accept incoming connections.
+///
+/// Connections can be accepted by calling [`accept`] on the TcpListener.
+///
+/// [`accept`]: TcpListener::accept
+pub struct TcpListener {
+    sock: Socket,
+}
+
+impl TcpListener {
+    /// Create a TCP listener bound to and listening on the provided socket address.
+    pub fn bind(addr: &SocketAddr) -> Result<Self> {
+        let domain = Domain::from_socket_addr(addr);
+
+        let mut sock = Socket::new(domain, SockType::Stream, Protocol::IpProtoTcp)?;
+        sock.bind(addr)?;
+        sock.listen()?;
+
+        Ok(Self { sock })
+    }
+
+    /// Accept a single connection from the TCP listener.
+    pub fn accept(&mut self) -> Result<(TcpStream, SocketAddr)> {
+        let (sock, peer) = self.sock.accept()?;
+        Ok((TcpStream { sock }, peer))
+    }
+}
+
+/// TCP Stream
+///
+/// This represents a connected TCP socket, which may be either a client or server socket. Data can
+/// be sent and received over the stream using the [`send`] and [`recv`] methods.
+///
+/// There are two ways to get a `TcpStream`:
+/// - Call [`accept`] on a [`TcpListener`] to get a connected stream as a server.
+/// - Use [`connect`] to get a connected stream as a client
+///
+/// [`accept`]: TcpListener::accept
+/// [`connect`]: TcpStream::connect
+/// [`send`]: TcpStream::send
+/// [`recv`]: TcpStream::recv
+pub struct TcpStream {
+    sock: Socket,
+}
+
+impl TcpStream {
+    /// Create a TCP stream by connecting to the provided socket address.
+    pub fn connect(addr: &SocketAddr) -> Result<Self> {
+        let domain = Domain::from_socket_addr(addr);
+
+        let mut sock = Socket::new(domain, SockType::Stream, Protocol::IpProtoTcp)?;
+        sock.connect(addr)?;
+
+        Ok(Self { sock })
+    }
+
+    /// Send data over the TCP stream, returning the number of bytes that were actually sent.
+    pub fn send(&mut self, buf: &[u8]) -> Result<usize> {
+        self.sock.send(buf)
+    }
+
+    /// Receive data from the TCP stream, returning the number of bytes received.
+    pub fn recv(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.sock.recv(buf)
     }
 }
