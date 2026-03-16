357270078719f5c9fcd20cb118e4be3426647016
David Brown <david.brown@linaro.org>
2026-03-15 18:07:37 -0600

    Support byte devicetree properties
    Add a runtime Bytes devicetree value so generated byte properties have a
    matching representation.
    
    Emit static byte slices from the devicetree generator instead of using
    vec!, which keeps the generated code valid in no_std builds.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- zephyr/src/lib.rs
+++ zephyr/src/lib.rs
@@ -132,6 +132,8 @@ pub mod devicetree {
     pub enum Value {
         /// A string value.
         String(&'static str),
+        /// An arbitrary byte sequence.
+        Bytes(&'static [u8]),
         /// A number value.
         Words(&'static [Word]),
         /// A phandle value.  For now, this is just the name, since we can't dynamically reference a module.
