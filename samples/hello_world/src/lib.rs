a72488b3a4f1b44cfcb578c0a4ffa12c90fd8592
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2026-05-06 11:25:55 +0200

    samples: hello_world: Document unsafe block
    Fixes issues raised by the Clippy CI tests.
    
    Signed-off-by: Emil Hammarstrom <emil.a.hammarstrom@gmail.com>

--- samples/hello_world/src/lib.rs
+++ samples/hello_world/src/lib.rs
@@ -7,6 +7,8 @@ use log::info;
 
 #[no_mangle]
 extern "C" fn rust_main() {
+    // SAFETY: `rust_main` runs once during application startup before any rust tasks
+    // are spawned, so the global logger is initialized before concurrent use.
     unsafe {
         zephyr::set_logger().unwrap();
     }
